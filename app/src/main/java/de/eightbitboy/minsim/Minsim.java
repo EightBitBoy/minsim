a317fd869e302413e16c3cbe9ec919e9f6159b6b
Stefan Härtel <haertel.dev@gmail.com>
2017-05-17 21:14:27 +0200

    Initialize the database

--- app/src/main/java/de/eightbitboy/minsim/Minsim.java
+++ app/src/main/java/de/eightbitboy/minsim/Minsim.java
@@ -2,17 +2,26 @@ package de.eightbitboy.minsim;
 
 import android.app.Application;
 
+import io.realm.Realm;
 import timber.log.Timber;
 
 public class Minsim extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initLogging();
+        initDatabase();
+    }
 
-        if(BuildConfig.DEBUG){
+    private void initLogging() {
+        if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
-
         Timber.d("### Logger has been initialized!");
+    }
+
+    private void initDatabase() {
+        Realm.init(getApplicationContext());
+        Timber.d("### Database has been initialized!");
     }
 }
--- app/src/main/java/de/eightbitboy/minsim/data/Level.java
+++ app/src/main/java/de/eightbitboy/minsim/data/Level.java
@@ -1,7 +1,9 @@
 package de.eightbitboy.minsim.data;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
-public class Level extends RealmObject{
+public class Level extends RealmObject {
+    @PrimaryKey
     public int number;
 }
