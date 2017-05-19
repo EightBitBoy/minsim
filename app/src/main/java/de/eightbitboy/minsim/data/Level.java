300e25a8ac57cfbca47fbad1b4e303e01f131ea8
Stefan Härtel <haertel.dev@gmail.com>
2017-05-19 10:38:03 +0200

    Add greenDAO stuff

--- app/src/main/java/de/eightbitboy/minsim/data/Level.java
+++ app/src/main/java/de/eightbitboy/minsim/data/Level.java
@@ -1,5 +1,11 @@
 package de.eightbitboy.minsim.data;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+
+@Entity
 public class Level {
+    @Id
+    public Long id;
     public int number;
 }
