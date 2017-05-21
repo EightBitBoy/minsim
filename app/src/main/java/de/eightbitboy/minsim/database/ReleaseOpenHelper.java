22a14442b4489d888d73149d46aafc336bf6ec7d
Stefan Härtel <haertel.dev@gmail.com>
2017-05-21 16:17:59 +0200

    Add new class for opening the database

---
+++ app/src/main/java/de/eightbitboy/minsim/database/ReleaseOpenHelper.java
@@ -0,0 +1,34 @@
+package de.eightbitboy.minsim.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.greenrobot.greendao.database.Database;
+
+import de.eightbitboy.minsim.data.DaoMaster;
+
+public class ReleaseOpenHelper extends DaoMaster.OpenHelper {
+    public ReleaseOpenHelper(Context context, String name) {
+        super(context, name);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        super.onCreate(db);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onUpgrade(db, oldVersion, newVersion);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
+}
