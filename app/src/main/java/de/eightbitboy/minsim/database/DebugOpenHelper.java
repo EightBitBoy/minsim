9133d14f7b3557d4acd0612d6f71f53824a5041b
Stefan Härtel <haertel.dev@gmail.com>
2017-05-22 12:09:45 +0200

    Implement database helper

--- app/src/main/java/de/eightbitboy/minsim/database/DebugOpenHelper.java
+++ app/src/main/java/de/eightbitboy/minsim/database/DebugOpenHelper.java
@@ -3,18 +3,16 @@ package de.eightbitboy.minsim.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
-
-
 import de.eightbitboy.minsim.data.DaoMaster;
 
-public class ReleaseOpenHelper extends DaoMaster.OpenHelper {
-    public ReleaseOpenHelper(Context context, String name) {
+
+public class DebugOpenHelper extends DaoMaster.DevOpenHelper{
+    public DebugOpenHelper(Context context, String name) {
         super(context, name);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         super.onCreate(db);
-        Database.seed(db);
     }
 }
