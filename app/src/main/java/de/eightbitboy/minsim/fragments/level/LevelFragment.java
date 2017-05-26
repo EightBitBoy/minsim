9cbded231668d6fb9e786a9b3eba2e2e2fd900ff
Stefan Härtel <haertel.dev@gmail.com>
2017-05-26 22:36:33 +0200

    Remove old code

--- app/src/main/java/de/eightbitboy/minsim/fragments/level/LevelFragment.java
+++ app/src/main/java/de/eightbitboy/minsim/fragments/level/LevelFragment.java
@@ -17,36 +17,14 @@ import de.eightbitboy.minsim.fragments.level.dummy.DummyContent.DummyItem;
 
 public class LevelFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public LevelFragment() {
-    }
-
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static LevelFragment newInstance(int columnCount) {
-        LevelFragment fragment = new LevelFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -54,15 +32,10 @@ public class LevelFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_level_item_list, container, false);
 
-        // Set the adapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
             recyclerView.setAdapter(new MyLevelItemRecyclerViewAdapter(DummyContent.ITEMS, mListener));
         }
         return view;
