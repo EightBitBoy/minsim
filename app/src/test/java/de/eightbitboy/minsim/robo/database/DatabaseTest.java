55680b85df6d7d5138498e0e323c21bf6d1bc60c
Stefan Härtel <haertel.dev@gmail.com>
2017-05-29 20:31:12 +0200

    Prepare database tests

--- app/src/test/java/de/eightbitboy/minsim/robo/database/DatabaseTest.java
+++ app/src/test/java/de/eightbitboy/minsim/robo/database/DatabaseTest.java
@@ -9,6 +9,8 @@ import org.robolectric.annotation.Config;
 import de.eightbitboy.minsim.BuildConfig;
 import de.eightbitboy.minsim.activities.MainActivity;
 import de.eightbitboy.minsim.database.Database;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 import static org.junit.Assert.*;
 
@@ -18,8 +20,32 @@ public class DatabaseTest {
 
     //TODO Read about setting up the activity for the whole class!
     @Test
-    public void initializeTest() {
+    void initializeTest() {
         MainActivity activity = Robolectric.setupActivity(MainActivity.class);
-        assertNotNull(Database.getSession());
+        Database.initialize(activity.getApplicationContext());
+        assertNotNull(Database.getDb());
+    }
+
+    @Test
+    void schema0Test() {
+        Realm db = Realm.getInstance(buildConfig(0));
+    }
+
+    @Test
+    void schema1Test() {
+        Realm db = Realm.getInstance(buildConfig(1));
+    }
+
+    @Test
+    void schema2Test() {
+        Realm db = Realm.getInstance(buildConfig(2));
+    }
+
+    RealmConfiguration buildConfig(long schemaVersion) {
+        return new RealmConfiguration.Builder()
+                .name("minsim_test.realm")
+                .schemaVersion(schemaVersion)
+                .inMemory()
+                .build();
     }
 }
