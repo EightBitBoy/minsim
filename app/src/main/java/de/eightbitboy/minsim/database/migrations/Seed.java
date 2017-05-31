120b11de250e890378ce66d247730689c344776f
Stefan Härtel <haertel.dev@gmail.com>
2017-06-01 01:40:00 +0200

    Add some logging

--- app/src/main/java/de/eightbitboy/minsim/database/migrations/Seed.java
+++ app/src/main/java/de/eightbitboy/minsim/database/migrations/Seed.java
@@ -2,10 +2,13 @@ package de.eightbitboy.minsim.database.migrations;
 
 import de.eightbitboy.minsim.data.Level;
 import io.realm.Realm;
+import timber.log.Timber;
 
 public class Seed implements Realm.Transaction {
     @Override
     public void execute(Realm realm) {
+        Timber.d("Seeding the database!");
+        
         Level level1 = new Level();
         level1.setId(1);
         level1.setHealth(10);
