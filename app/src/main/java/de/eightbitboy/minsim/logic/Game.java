d2508723576f2a81f56dd4b601e2c5b67febc14b
Stefan Härtel <haertel.dev@gmail.com>
2017-06-01 22:23:29 +0200

    Add game code

--- app/src/main/java/de/eightbitboy/minsim/logic/Game.java
+++ app/src/main/java/de/eightbitboy/minsim/logic/Game.java
@@ -2,8 +2,21 @@ package de.eightbitboy.minsim.logic;
 
 import android.content.Context;
 
-public class Game {
-    public static void initialize(Context context) {
+import io.realm.Realm;
 
-    }
+public class Game {
+
+	private static Game INSTANCE;
+
+	private Game() {
+		Realm db = Realm.getDefaultInstance();
+	}
+
+	public static void initialize(Context context) {
+		INSTANCE = new Game();
+	}
+
+	public static Game get() {
+		return INSTANCE;
+	}
 }
