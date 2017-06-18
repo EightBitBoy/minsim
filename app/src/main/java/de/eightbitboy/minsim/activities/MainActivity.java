b790bdf1e8311405bf8b8abadf754d744fc3c4b4
Stefan Härtel <haertel.dev@gmail.com>
2017-06-18 13:25:20 +0200

    Add toolbar menu

--- app/src/main/java/de/eightbitboy/minsim/activities/MainActivity.java
+++ app/src/main/java/de/eightbitboy/minsim/activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import butterknife.BindView;
@@ -59,5 +60,11 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
     }
 }
