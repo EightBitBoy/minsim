7b91cb57df8be1114bc8813c51f85dac7f5d43f5
Stefan Härtel <haertel.dev@gmail.com>
2017-06-22 20:16:26 +0200

    Remove fabric stuff

--- app/src/main/java/de/eightbitboy/minsim/Minsim.java
+++ app/src/main/java/de/eightbitboy/minsim/Minsim.java
@@ -2,16 +2,13 @@ package de.eightbitboy.minsim;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
 import de.eightbitboy.minsim.database.Database;
-import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class Minsim extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Fabric.with(this, new Crashlytics());
 		initLogging();
 		initTestingGround();
 		initDatabase();
