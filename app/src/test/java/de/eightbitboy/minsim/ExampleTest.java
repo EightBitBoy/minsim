33de00366ddf75e3e4520370434611d0964932b3
Stefan Härtel <haertel.dev@gmail.com>
2017-08-18 20:38:20 +0200

    Rename file

--- app/src/test/java/de/eightbitboy/minsim/ExampleTest.java
+++ app/src/test/java/de/eightbitboy/minsim/ExampleTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class ExampleUnitTest {
+public class ExampleTest {
 
     @Test
     public void addition_isCorrect() throws Exception {
