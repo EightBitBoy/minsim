05ec37c759ca1f9cd20b88539e05a1d3ccd73d49
Stefan Härtel <haertel.dev@gmail.com>
2017-08-18 20:55:36 +0200

    Implement test

--- app/src/test/groovy/de/eightbitboy/minsim/ExampleSpec.groovy
+++ app/src/test/groovy/de/eightbitboy/minsim/ExampleSpec.groovy
@@ -1,3 +1,16 @@
-public class ExampleSpec {
+import spock.lang.Specification
 
+public class ExampleSpec extends Specification{
+
+    def "add values"(){
+        setup:
+        int valueA = 5
+        int valueB = 8
+
+        when:
+        int result = valueA + valueB
+
+        then:
+        result == 13
+    }
 }
