23c6e0d679d7484fd5645218d4e3c339af4bf727
Stefan Härtel <haertel.dev@gmail.com>
2017-08-18 21:08:44 +0200

    Add test code

--- app/src/test/groovy/de/eightbitboy/minsim/robo/MainActivitySpec.groovy
+++ app/src/test/groovy/de/eightbitboy/minsim/robo/MainActivitySpec.groovy
@@ -1,7 +1,19 @@
-package de.eightbitboy.minsim.robo;
+package de.eightbitboy.minsim.robo
 
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import spock.lang.Specification
 
+import de.eightbitboy.minsim.BuildConfig
+import de.eightbitboy.minsim.activities.MainActivity
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class MainActivitySpec extends Specification{
 
+    def "the navigation is available"(){
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+    }
 }
