b713297bdfee0f7baa142b053a281a2506f491f8
Stefan Härtel <haertel.dev@gmail.com>
2019-01-07 20:58:30 +0100

    Add db code

--- app/src/main/java/de/eightbitboy/minsim/data/Db.kt
+++ app/src/main/java/de/eightbitboy/minsim/data/Db.kt
@@ -1,5 +1,11 @@
 package de.eightbitboy.minsim.data
 
-class Db {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import de.eightbitboy.minsim.data.entities.Mine
+import de.eightbitboy.minsim.data.entities.MineDao
 
+@Database(entities = [Mine::class], version = 1)
+abstract class Db : RoomDatabase() {
+	abstract fun mineDao(): MineDao
 }
