70465c7a4ffd51c199a3ef7e59d9cbf60cfe06a9
Stefan Härtel <haertel.dev@gmail.com>
2019-06-25 00:44:03 +0200

    Test works

--- logic/src/test/groovy/de/eightbitboy/minsim/logic/GameTest.groovy
+++ logic/src/test/groovy/de/eightbitboy/minsim/logic/GameTest.groovy
@@ -3,15 +3,8 @@ package de.eightbitboy.minsim.logic
 import spock.lang.Specification
 
 class GameTest extends Specification {
-    "foobar"() {
-        given:
-        def a = 10
-        def b = 30
-
-        when:
-        def c = a + b
-
-        then:
-        c == 41
+    def "foo"() {
+        expect:
+        ["a", "b"].contains("a")
     }
 }
