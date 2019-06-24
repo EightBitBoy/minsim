79023d13568be601417e32c0f87243c5a0288cfe
Stefan Härtel <haertel.dev@gmail.com>
2019-06-25 01:29:07 +0200

    Add test

--- logic/src/main/java/de/eightbitboy/minsim/logic/Game.kt
+++ logic/src/main/java/de/eightbitboy/minsim/logic/Game.kt
@@ -1,6 +1,10 @@
 package de.eightbitboy.minsim.logic
 
 class Game {
-    fun tick() {
+
+    var tick: Long = 0
+
+    fun doTick() {
+        tick = tick.inc()
     }
 }
--- logic/src/test/groovy/de/eightbitboy/minsim/logic/GameTest.groovy
+++ logic/src/test/groovy/de/eightbitboy/minsim/logic/GameTest.groovy
@@ -3,13 +3,19 @@ package de.eightbitboy.minsim.logic
 import spock.lang.Specification
 
 class GameTest extends Specification {
-    def "foo"() {
-        expect:
-        ["a", "b"].contains("a")
+    def "The tick counter is incremented."() {
+        given:
+        def game = new Game()
+
+        when:
+        game.doTick()
+
+        then:
+        game.tick == 1
     }
 
     def "bar"() {
         expect:
-        assert 3 + 5 == 9
+        assert 3 + 5 == 8
     }
 }
